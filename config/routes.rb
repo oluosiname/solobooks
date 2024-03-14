f3e8913b10638d53c5d1f95afb0a97f4e94f9d97
Olumuyiwa Osiname <oluosiname@gmail.com>
2024-03-14 20:50:21 +0100

    setup devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,22 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users,
+  path: '', # optional namespace or empty string for no space
+  path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    password: 'password',
+    confirmation: 'verification',
+    # registration: 'register',
+    sign_up: 'register',
+  }
+  # controllers: { registrations: 'registrations' }
+  
+  # Defines the root path route ("/")
+  scope '(:locale)', locale: /en|de/ do
+    root 'home#index'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
