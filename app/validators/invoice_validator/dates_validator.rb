d2cd0eae8972afdcf464d9ec7d89c5b676364be5
Olumuyiwa Osiname <oluosiname@gmail.com>
2024-03-18 20:11:21 +0100

    fix date validator bug

--- app/validators/invoice_validator/dates_validator.rb
+++ app/validators/invoice_validator/dates_validator.rb
@@ -2,6 +2,8 @@
 
 class InvoiceValidator::DatesValidator < ActiveModel::Validator
   def validate(record)
+    return if record.date.blank? || record.due_date.blank?
+
     if record.due_date < record.date
       record.errors.add :due_date, 'must be after the date'
     end
