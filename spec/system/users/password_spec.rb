80a07782559ca4a6ba1416432947fc2e5d1c7a74
Olumuyiwa Osiname <oluosiname@gmail.com>
2024-04-11 21:43:49 +0200

    design and implement password reset

---
+++ spec/system/users/password_spec.rb
@@ -0,0 +1,50 @@
+
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Password Management', type: :system do
+  context 'requesting password reset' do
+    let!(:user) { create(:user, :confirmed, email: 'test@example.com', password: 'password') }
+
+    it 'requests password change' do
+      visit new_user_session_path
+
+      click_on 'Forgot your password?'
+
+      fill_in 'user[email]', with: user.email
+      click_on 'Reset Password'
+
+      expect(page).to have_content('If your email address exists in our database, you will receive a password recovery link at your email address in a few minutes')
+    end
+  end
+
+  context 'updating password' do
+    let!(:user) { create(:user, :confirmed, email: 'test@example.com', password: 'password') }
+
+    it 'updates password' do
+      visit edit_user_password_path(reset_password_token: user.send_reset_password_instructions)
+
+      fill_in 'user[password]', with: 'new-password'
+      fill_in 'user[password_confirmation]', with: 'new-password'
+      click_on 'Change password'
+
+      expect(page).to have_content('Your password has been changed successfully. You are now signed in.')
+      expect(page).to have_current_path(root_path)
+    end
+
+    context 'with invalid token' do
+      it 'does not update password' do
+        visit edit_user_password_path(reset_password_token: 'invalid-token')
+        
+        fill_in 'user[password]', with: 'new-password'
+        fill_in 'user[password_confirmation]', with: 'new-password'
+
+        click_on 'Change password'
+
+        expect(page).to have_current_path(user_password_path)
+        expect(page).to have_content('Not saved')
+      end
+    end
+  end
+end
