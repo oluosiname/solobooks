a0f429fb37c2fd363d917ff2da2ad1ca4c9c8c02
Olumuyiwa Osiname <oluosiname@gmail.com>
2024-04-13 15:40:57 +0200

    implement expense creation

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -5,6 +5,24 @@ class ExpensesController < ApplicationController
     @expense = current_user.expenses.build
   end
 
+  def create
+    @expense = current_user.expenses.build(expense_params)
+
+    if @expense.save
+      respond_to do |format|
+        format.html do
+          redirect_to transactions_path, notice: t('record.create.success', resource: Expense.model_name.human)
+        end
+        format.turbo_stream { flash.now[:notice] = t('record.create.success', resource: Expense.model_name.human) }
+      end
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   private
 
+  def expense_params
+    params.require(:expense).permit(:amount, :date, :description)
+  end
 end
---
+++ spec/system/financial_transactions/expense_spec.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Expense', type: :system do
+  describe 'creating expense' do
+    before do
+      create(:user, :confirmed)
+      login_user
+    end
+
+    it 'allows users to Login' do
+      visit transactions_path
+
+      click_on 'Add Expense'
+
+      fill_in 'expense[amount]', with: Faker::Number.decimal(l_digits: 2)
+      fill_in 'expense[description]', with: 'Test Expense'
+      fill_in 'expense[date]', with: Faker::Date.between(from: 1.year.ago, to: Time.zone.today)
+      click_on 'Save'
+
+      expect(page).to have_content('Expense was successfully created')
+      expect(page).to have_current_path(transactions_path)
+    end
+
+    context 'with invalid data' do
+      it 'displays error messages' do
+        visit transactions_path
+
+        click_on 'Add Expense'
+
+        fill_in 'expense[amount]', with: Faker::Number.decimal(l_digits: 2)
+        click_on 'Save'
+
+        expect(page).to have_content('1 error prohibited this Expense from being saved')
+        expect(page).to have_content("Date can't be blank")
+      end
+    end
+  end
+end
