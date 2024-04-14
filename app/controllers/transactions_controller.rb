f8e4512ccb08a5b6333963fe8a19a9b652e49ffe
Olumuyiwa Osiname <oluosiname@gmail.com>
2024-04-14 17:08:39 +0200

    refactor expenses creation to use transactions new

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,7 +1,25 @@
 # frozen_string_literal: true
 
 class TransactionsController < ApplicationController
+  before_action :build_transaction, only: [:new]
+
   def index
     @transactions = current_user.financial_transactions
+  end
+
+  def new; end
+
+  private
+
+  def build_transaction
+    @transaction = send("build_#{params[:transaction_type]}")
+  end
+
+  def build_expense
+    current_user.expenses.build
+  end
+
+  def build_income
+    current_user.incomes.build
   end
 end
