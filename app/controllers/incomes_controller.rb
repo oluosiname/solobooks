63d6e87d7ce1e9a16d4c7990f054df94c3246a58
Olumuyiwa Osiname <oluosiname@gmail.com>
2024-04-14 21:32:42 +0200

    add tests for financial transaction page

--- app/controllers/incomes_controller.rb
+++ app/controllers/incomes_controller.rb
@@ -9,7 +9,11 @@ class IncomesController < ApplicationController
         format.html do
           redirect_to transactions_path, notice: t('record.create.success', resource: Income.model_name.human)
         end
-        format.turbo_stream { flash.now[:notice] = t('record.create.success', resource: Income.model_name.human) }
+        format.turbo_stream do
+          # TODO: Decide if we are replacing the whole list or just adding the new record
+          # @transactions = current_user.financial_transactions.order(date: :desc)
+          flash.now[:notice] = t('record.create.success', resource: Income.model_name.human)
+        end
       end
     else
       render 'transactions/new', status: :unprocessable_entity
---
+++ spec/system/financial_transactions/transaction_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Transactions', type: :system do
+  describe 'page' do
+    before do
+      create(:profile, user:)
+      login_user(user)
+    end
+
+    let(:user) { create(:user, :confirmed) }
+    let!(:income) { create(:income, user:) }
+    let!(:expense) { create(:expense, user:) }
+
+    it 'displays incomes and expenses' do
+      visit transactions_path
+
+      within('#transactions-list') do
+        expect(page).to have_content(income.description)
+        expect(page).to have_content(income.amount)
+        expect(page).to have_content(income.date)
+        expect(page).to have_content(expense.description)
+        expect(page).to have_content(expense.amount)
+        expect(page).to have_content(expense.date)
+      end
+    end
+
+    it 'has link to create new transaction' do
+      visit transactions_path
+
+      expect(page).to have_link('Add Income', href: new_transaction_path(transaction_type: 'income'))
+      expect(page).to have_link('Add Expense', href: new_transaction_path(transaction_type: 'expense'))
+    end
+  end
+end
