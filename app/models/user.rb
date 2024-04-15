da0c68293dba6e41daf83b320d5b095e4ce8e31d
Olumuyiwa Osiname <oluosiname@gmail.com>
2024-04-15 19:34:01 +0200

    add payment detail model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   has_many :invoices, dependent: :destroy
   has_many :clients, dependent: :destroy
   has_one :profile, dependent: :destroy
+  has_one :payment_detail, dependent: :destroy
   has_many :expenses, dependent: :destroy
   has_many :incomes, dependent: :destroy
   has_many :financial_transactions, dependent: :destroy
