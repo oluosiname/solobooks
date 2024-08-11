2ec22e222a6b763b335b001f944d8fc6eb58d176
Olumuyiwa Osiname <oluosiname@gmail.com>
2024-08-11 08:11:11 +0200

    add settings model

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,6 +3,7 @@
 class Profile < ApplicationRecord
   belongs_to :user
   belongs_to :currency
+  has_one :setting, dependent: :destroy
   has_one :address, as: :addressable, dependent: :destroy
   accepts_nested_attributes_for :address, allow_destroy: true, reject_if: :all_blank
 
