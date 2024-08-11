8cfb2c335a324b79758de3580231b18411c6f02d
Olumuyiwa Osiname <oluosiname@gmail.com>
2024-08-11 14:54:17 +0200

    fix tests

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -4,4 +4,9 @@ class Setting < ApplicationRecord
   belongs_to :profile
   has_one :user, through: :profile
   belongs_to :currency
+
+  LANGUAGES = {
+    'English' => 'en',
+    'Deutsch' => 'de',
+  }
 end
