f7cd0694ea146ad723170c91a346e1bccb83f9f4
Olumuyiwa Osiname <oluosiname@gmail.com>
2024-08-14 09:02:14 +0200

    close dropdown on outside click

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -3,7 +3,18 @@ import { Controller } from "@hotwired/stimulus";
 // Connects to data-controller="dropdown"
 export default class extends Controller {
   static targets = ["dropdownButton", "dropdownContainer"];
-  connect() {}
+  connect() {
+    // Bind the handleClickOutside method to the instance of this class
+    this.handleClickOutside = this.handleClickOutside.bind(this);
+
+    // Listen for clicks outside the dropdown
+    document.addEventListener("click", this.handleClickOutside);
+  }
+
+  disconnect() {
+    // Clean up the event listener when the controller is disconnected
+    document.removeEventListener("click", this.handleClickOutside);
+  }
 
   actionToggle(e) {
     document.querySelectorAll(".dropdown").forEach((dropdown) => {
@@ -12,5 +23,12 @@ export default class extends Controller {
       }
     });
     this.dropdownContainerTarget.classList.toggle("open");
+  }
+
+  handleClickOutside(e) {
+    // Check if the click was outside the dropdownContainer
+    if (!this.dropdownContainerTarget.contains(e.target)) {
+      this.dropdownContainerTarget.classList.remove("open");
+    }
   }
 }
