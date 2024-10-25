89944d3574b5c3bdb86b93bfc93fce9e838b2247
Olumuyiwa Osiname <oluosiname@gmail.com>
2024-10-25 21:55:36 +0200

    remove test mailer

--- spec/mailers/previews/test_preview.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# Preview all emails at http://localhost:3000/rails/mailers/test
-class TestPreview < ActionMailer::Preview
-  # Preview this email at http://localhost:3000/rails/mailers/test/hello
-  def hello
-    TestMailer.hello
-  end
-end
