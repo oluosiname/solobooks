f65008398fc3f56db04054a2c1d4c89ab973a8bc
Olumuyiwa Osiname <oluosiname@gmail.com>
2024-10-25 22:18:54 +0200

    debug postmark

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,6 +2,13 @@
 
 module Users
   class RegistrationsController < Devise::RegistrationsController
+    def new
+      Rails.logger.info "Postmark API Token: #{ENV["POSTMARK_API_TOKEN"]}"
+      build_resource
+      yield resource if block_given?
+      respond_with resource
+    end
+
     def after_inactive_sign_up_path_for(resource)
       confirmation_instructions_sent_path
     end
