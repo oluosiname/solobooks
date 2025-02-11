c1ec35c3e99f7e971115c0ec419d07a43c1d68d2
Olumuyiwa Osiname <oluosiname@gmail.com>
2025-02-11 21:28:08 +0100

    add subdomain

--- config/routes.rb
+++ config/routes.rb
@@ -4,50 +4,58 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 
-  scope '(:locale)', locale: /en|de/ do
-    devise_for :users,
-      path: '', # optional namespace or empty string for no space
-      path_names: {
-        sign_in: 'login',
-        sign_out: 'logout',
-        password: 'password',
-        confirmation: 'verification',
-        # registration: 'register',
-        sign_up: 'register',
-      },
-      controllers: {
-        confirmations: 'users/confirmations',
-        registrations: 'users/registrations',
-      }
+  constraints subdomain: '' do
+    scope '(:locale)', locale: /en|de/ do
+      get 'landing', to: 'landing_pages#show'
+    end
+  end
 
-    root 'home#index'
+  constraints subdomain: 'app' do
+    scope '(:locale)', locale: /en|de/ do
+      devise_for :users,
+        path: '', # optional namespace or empty string for no space
+        path_names: {
+          sign_in: 'login',
+          sign_out: 'logout',
+          password: 'password',
+          confirmation: 'verification',
+          # registration: 'register',
+          sign_up: 'register',
+        },
+        controllers: {
+          confirmations: 'users/confirmations',
+          registrations: 'users/registrations',
+        }
 
-    resources :invoices, only: [:new, :create, :index] do
-      get :vat_technique, on: :collection
-      member do
-        patch :send_invoice
-        patch :pay
-        patch :cancel
-        patch :mark_overdue
-        patch :send_reminder
+      root 'home#index'
+
+      resources :invoices, only: [:new, :create, :index] do
+        get :vat_technique, on: :collection
+        member do
+          patch :send_invoice
+          patch :pay
+          patch :cancel
+          patch :mark_overdue
+          patch :send_reminder
+        end
       end
-    end
-    resources :invoice_categories, only: [:new, :create]
-    resources :clients, only: [:new, :create, :index, :destroy] do
-      get :new_modal, on: :collection
-    end
-    resource :profile, only: [:show, :update]
-    post :profile, to: 'profiles#create', as: :create_profile
+      resources :invoice_categories, only: [:new, :create]
+      resources :clients, only: [:new, :create, :index, :destroy] do
+        get :new_modal, on: :collection
+      end
+      resource :profile, only: [:show, :update]
+      post :profile, to: 'profiles#create', as: :create_profile
 
-    resources :expenses, only: [:new, :create]
-    resources :incomes, only: [:new, :create]
-    resources :transactions, only: [:new, :edit, :update, :index, :destroy], as: :transactions
+      resources :expenses, only: [:new, :create]
+      resources :incomes, only: [:new, :create]
+      resources :transactions, only: [:new, :edit, :update, :index, :destroy], as: :transactions
 
-    resources :payment_details, only: [:update, :create]
-    resources :settings, only: [:index, :update, :create]
+      resources :payment_details, only: [:update, :create]
+      resources :settings, only: [:index, :update, :create]
 
-    devise_scope :user do
-      get 'instructions_sent', to: 'users/confirmations#instructions_sent', as: :confirmation_instructions_sent
+      devise_scope :user do
+        get 'instructions_sent', to: 'users/confirmations#instructions_sent', as: :confirmation_instructions_sent
+      end
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
